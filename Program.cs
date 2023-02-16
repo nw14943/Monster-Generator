af8a2c4bbce60a821a74660057fde205d2988b0d
Nathan Wood <woo20023@byui.edu>
2023-02-16 13:45:06 -0700

    Monster and StatBlocks

---
+++ Program.cs
@@ -0,0 +1,166 @@
+﻿// This is a top-level statement.
+// It is essentially the main function.
+Console.WriteLine("Program Start");
+
+Monster m1 = new Monster();
+m1.display();
+m1.display(false);
+Console.WriteLine(m1);
+
+Monster m2 = new Monster("Monster");
+m2.display();
+m2.display(false);
+Console.WriteLine(m2);
+
+Monster m3 = new Monster("Adult Red Dragon");
+m3.display();
+m3.display(false);
+Console.WriteLine(m2);
+
+Console.WriteLine("Program End");
+
+
+class Monster
+{
+    public Monster()
+    {
+        this.name = "Unnamed";
+        this.statBlock = new StatBlock();
+    }
+    public Monster(string name)
+    {
+        this.name = name;
+        this.statBlock = new StatBlock(name);
+    }
+    public override string ToString()
+    {
+        return $"{this.name}";
+    }
+    private string name;
+    public StatBlock statBlock;
+
+    public string getName()
+    {
+        return this.name;
+    }
+    public void display(bool limit = true)
+    {
+        Console.WriteLine(this);
+        this.statBlock.display(limit);
+    }
+}
+class StatBlock
+{
+    public StatBlock()
+    {
+        setByName("");
+    }
+    public StatBlock(string name)
+    {
+        setByName(name);
+    }
+    public override string ToString()
+    {
+        return 
+            $"  CR: {this.cr}\n" +
+            $"  Type: {this.size} {this.type}\n" +
+            $"  Alignment: {this.alignment}";
+    }
+
+    private int cr;
+    private Size size;
+    private string type;
+    private Alignment alignment;
+    private int ac;
+    private int hp;
+    private Stats stats;
+    public void display(bool limit = true)
+    {
+        Console.WriteLine(this);
+        if (!limit)
+        {
+            Console.WriteLine($"    AC: {this.ac}");
+            Console.WriteLine($"    Hit Points: {this.hp}");
+            Console.WriteLine($"    Stats:\n    {this.stats}");
+        }
+    }
+    public void setByName(string name)
+    {
+        switch (name)
+        {
+        case "Monster":
+            this.cr = 1;
+            this.size = Size.Large;
+            this.type = "Monstrosity";
+            this.alignment = Alignment.NN;
+            this.ac = 15;
+            this.hp = 30;
+            this.stats = new Stats(15);
+            break;
+        case "Adult Red Dragon":
+            this.cr = 17;
+            this.size = Size.Huge;
+            this.type = "Dragon";
+            this.alignment = Alignment.CE;
+            this.ac = 19;
+            this.hp = 256;
+            this.stats = new Stats(27, 10, 25, 16, 13, 21);
+            break;
+        default:
+            this.cr = -1;
+            this.size = Size.None;
+            this.type = "None";
+            this.alignment = Alignment.None;
+            this.ac = 0;
+            this.hp = 0;
+            this.stats = new Stats();
+            break;
+        }
+    }
+}
+
+class Stats 
+{
+    public Stats()
+    {
+        this.strength     = 0;
+        this.dexerity     = 0;
+        this.constitution = 0;
+        this.intelegence  = 0;
+        this.wisdom       = 0;
+        this.charisma     = 0;
+    }
+    public Stats(int value)
+    {
+        this.strength     = value;
+        this.dexerity     = value;
+        this.constitution = value;
+        this.intelegence  = value;
+        this.wisdom       = value;
+        this.charisma     = value;
+    }
+    public Stats(int s, int d, int c, int i, int w, int ch)
+    {
+        this.strength     = s;
+        this.dexerity     = d;
+        this.constitution = c;
+        this.intelegence  = i;
+        this.wisdom       = w;
+        this.charisma     = ch;
+    }
+    public override string ToString()
+    {
+        return 
+        $"  STR:{this.strength} DEX:{this.dexerity} CON:{this.constitution}\n" +
+    $"      INT:{this.intelegence} WIS:{this.wisdom} CHA:{this.charisma}";
+    }
+    private int strength;
+    private int dexerity;
+    private int constitution;
+    private int intelegence;
+    private int wisdom;
+    private int charisma;
+}
+
+enum Size {None, Small, Tiny, Medium, Large, Huge, Gargantuan, Titan}
+enum Alignment {None, CG, NG, LG, CN, NN, LN, CE, NE, LE}
---
+++ csProgram.md
@@ -0,0 +1,30 @@
+# Overview
+
+I have wanted to create some DnD tools for DMs. This is one section of
+that project. I also wanted to get to know C# so that is the reason that
+this section is written in C#.
+
+A link to your YouTube demonstration. It is a 4-5 minute demo of the software running and a walkthrough of the code. (eventually)
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Development Environment
+
+I used VScode for this project. It seemed like it would be a simple
+environment for this program.
+
+C# is an object-oriented language. It is primarily used in apps and games.
+
+# Useful Websites
+
+A list of websites that I found helpful in this project.
+
+- [C# Documentation](https://learn.microsoft.com/en-us/dotnet/csharp/)
+- [C# Tutorial W3Schools](https://www.w3schools.com/cs/index.php)
+
+# Future Work
+
+A list of things that I need to fix, improve, and add in the future.
+
+- Read monsters from files rather than hard coded.
+- Make a randomizer for specific challenge ratings.
